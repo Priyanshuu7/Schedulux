011fb5bc1db86b1b68958fefa54c5fd58e7f07d4
Priyanshuu7 <rajakpriyanshu581@gmail.com>
2025-06-01 23:43:55 +0530

    Added Auth,Navbar,Sessoin

---
+++ nextjs-app/app/components/Navbar.tsx
@@ -0,0 +1,17 @@
+import Image from "next/image";
+import Link from "next/link";
+import Logo from "@/public/logo.png";
+
+export function Navbar() {
+  return (
+    <div className="flex py-5 items-center justify-between">
+      <Link href="/" className="flex items-center gap-2" >
+        <Image src={Logo} alt="Logo" className="size-10" />
+        <h4 className="text-2xl font-bold">
+        Saas <span className="text-blue-500">App</span>
+      </h4>
+      </Link>
+      
+    </div>
+  );
+}
---
+++ nextjs-app/app/dashboard/page.tsx
@@ -0,0 +1,7 @@
+import requireUser from "../lib/hooks"
+
+
+export default async function Dashboard() {
+  const session = await requireUser()
+  return <div>Dashboard</div>
+}
