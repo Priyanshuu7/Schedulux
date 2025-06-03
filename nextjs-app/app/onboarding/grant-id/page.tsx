8dbcb986af7e6dba24c8b67ed9eb50c13f33b8f2
Priyanshuu7 <rajakpriyanshu581@gmail.com>
2025-06-03 19:45:17 +0530

    Connected nylas auth

---
+++ nextjs-app/app/onboarding/grant-id/page.tsx
@@ -0,0 +1,49 @@
+import {
+    Card,
+    CardHeader,
+    CardTitle,
+    CardDescription,
+    CardContent,
+  } from "@/components/ui/card";
+  import Image from "next/image";
+  import VideoGif from "@/public/work-is-almost-over-happy.gif"; // adjust path if needed
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { CalendarCheck2 } from "lucide-react";
+  
+  export default function OnboardingRoutetwo() {
+    return (
+      <div className="min-h-screen w-screen flex items-center justify-center bg-gray-50">
+        <Card className="w-full max-w-md shadow-md">
+          <CardHeader>
+            <CardTitle className="text-2xl font-semibold">
+              You Are Almost Done!
+            </CardTitle>
+            <CardDescription className="text-sm text-muted-foreground">
+              We now need to connect your calendar to your account.
+            </CardDescription>
+            <Image
+              src={VideoGif}
+              alt="Calendar connection demo"
+              className="mx-auto rounded-md"
+              width={260} // or adjust this to the actual image width you want
+              height={180}
+            /> 
+          </CardHeader>
+  
+          <CardContent>
+            <Button className="w-full" asChild>
+                <Link href = "/api/auth ">
+                <CalendarCheck2 className="size- mr-2"/>
+                Connect Your Calender Account
+                
+                </Link>
+                
+            </Button>
+          
+          </CardContent>
+        </Card>
+      </div>
+    );
+  }
+  
