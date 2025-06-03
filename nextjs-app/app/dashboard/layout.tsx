1365351bd43c09e1b2fa08dab6ada3f9f3817eb6
Priyanshuu7 <rajakpriyanshu581@gmail.com>
2025-06-03 11:23:53 +0530

    Updated /dashboard/layout.tsx

--- nextjs-app/app/dashboard/layout.tsx
+++ nextjs-app/app/dashboard/layout.tsx
@@ -104,6 +104,7 @@ const session = await requireUser();
 
             </div>
           </header>
+          {/* here we rendered the page.tsx */}
           <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
             {children}
           </main>
